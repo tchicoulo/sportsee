c63702f093edae007227965f38b4cb090e0f818e
Calixte <tchicoulo@hotmail.fr>
2022-07-29 18:20:10 +0200

    added 3rd chart

--- src/components/Performances.jsx
+++ src/components/Performances.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from "recharts";
 
 const Performances = ({ data }) => {
-  // console.log(data);
+  let dataKindValues = [];
+  dataKindValues.push(Object.values(data.kind));
+  let dataCopied = { ...data };
+  // dataCopied.data.splice(0, 5, dataKindValues);
+  console.log(dataCopied);
+
   return (
     <div className="performances">
-      <h2>performances</h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data.data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey={"kind"} />
+          <PolarRadiusAxis axisLine={false} />
+          <Radar dataKey="value" fill="#FF0101" fillOpacity={0.7} />
+        </RadarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
