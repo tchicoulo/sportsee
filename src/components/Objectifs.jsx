c46a9b32f47b55ef3870cf65bc716c0c0cbabadc
Calixte <tchicoulo@hotmail.fr>
2022-08-18 15:20:32 +0200

    pie chart completed 100%

--- src/components/Objectifs.jsx
+++ src/components/Objectifs.jsx
@@ -8,8 +8,52 @@ const Objectifs = ({ data }) => {
 
   const dataPie = [
     { name: "Active Value", value: dataScore },
-    { name: "Inactive Value", value: 0.2 },
+    { name: "Inactive Value", value: 0.88 },
   ];
+
+  const dataPieInner = [{ name: "Active Value", value: 1 }];
+
+  const CustomLabel = ({ viewBox, noOfBubbleTeaSold = 0 }) => {
+    const { cx, cy } = viewBox;
+    return (
+      <React.Fragment>
+        <text x={cx - 20} y={cy - 5}>
+          <tspan
+            style={{
+              fontWeight: 700,
+              fontSize: "26px",
+              fill: "#282D30",
+              // fontFamily: "Roboto",
+            }}
+          >
+            {noOfBubbleTeaSold}
+          </tspan>
+        </text>
+        <text x={cx - 25} y={cy + 25}>
+          <tspan
+            style={{
+              fontWeight: 500,
+              fontSize: "16px",
+              fill: "#74798C",
+            }}
+          >
+            de votre
+          </tspan>
+        </text>
+        <text x={cx - 25} y={cy + 50}>
+          <tspan
+            style={{
+              fontWeight: 500,
+              fontSize: "16px",
+              fill: "#74798C",
+            }}
+          >
+            objectif
+          </tspan>
+        </text>
+      </React.Fragment>
+    );
+  };
 
   return (
     <div className="objectifs">
@@ -17,33 +61,38 @@ const Objectifs = ({ data }) => {
       <ResponsiveContainer width="100%" height="100%">
         <PieChart width={160} height={160}>
           <Pie
+            cornerRadius={20}
             data={dataPie}
             dataKey={"value"}
             cx="50%"
             cy="50%"
             innerRadius={80}
             outerRadius={90}
-            fill="#82ca9d"
             style={{
               WebkitTransform: "rotate(-30deg)",
               transformOrigin: "center",
-              BackgroundColor: "white",
             }}
           >
             {dataPie.map((entry, index) => {
               if (index === 1) {
                 return <Cell key={`cell-${index}`} fill="#FBFBFB" />;
-                // make sure to map the index to the colour you want
               }
               return <Cell key={`cell-${index}`} fill="#FF0000" />;
             })}
+          </Pie>
+          <Pie
+            data={dataPieInner}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#FFF"
+          >
             <Label
-              value={`${dataPie[0].value * 100}%`}
+              content={
+                <CustomLabel noOfBubbleTeaSold={`${dataPie[0].value * 100}%`} />
+              }
               position="center"
-              fill="#282D30"
-              style={{
-                fontSize: "26px",
-              }}
             />
           </Pie>
         </PieChart>
